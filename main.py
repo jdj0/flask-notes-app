98ea736fb3b1ed715a7f8bbd666582def7fb0299
jdj0 <joshua.davidjones92@gmail.com>
2022-06-16 18:22:10 +0000

    Create auth.py routes

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-from website import creat_app
+from website import create_app
+
+app = create_app()
+
+# Only run the webserver if we are running this file. app.run() starts the server
+if __name__ == '__main__':
+    app.run(debug=True)
--- website/__init__.py
+++ website/__init__.py
@@ -4,4 +4,11 @@ def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "sneaky secret key"
 
+    from .views import views
+    from .auth import auth
+
+    # registering blueprints
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
+
     return app
